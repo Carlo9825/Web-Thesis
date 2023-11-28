773521f2b7d4fb62401b2a0aecb9e3e77924cd48
JuAnCalBen <121489018+JuAnCalBen@users.noreply.github.com>
2023-11-28 17:22:53 +0100

    changes in Javascript
    Juan. 17:22  of 28/11

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 function show() {
 	alert("hola");
 	}
+
+
+
+function active_canvas  (ide) {
+    // alert("hola");
+    var padre = document.getElementById("contnt");
+    var elementosHijosDiv = padre.children;
+
+    for (var i = 0; i < elementosHijosDiv.length; i++) {
+           elementosHijosDiv[i].style.display = "none";
+           
+    }
+    var elemento = document.getElementById(ide);
+    elemento.style.display="block"
+    
+}
